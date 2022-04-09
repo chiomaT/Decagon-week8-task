220e010abe2967beaf5e4a095c2779d0e94478b3
durotemy <durotemy@gmail.com>
2022-04-09 23:03:12 +0100

    Admin landing page

--- job-app/src/App.js
+++ job-app/src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
-import ApplicationForm from './Components/ApplicationForm'
+// import ApplicationForm from './Components/ApplicationForm'
 import './App.css';
+import Header from './Components/Admin/Header';
+import Input from './Components/Admin/Input';
+import Container from './Components/Admin/Container';
+import Footer from './Components/Admin/Footer';
 
 function App() {
   return (
     <div className="App">
-      <ApplicationForm />
+      {/* <ApplicationForm /> */}
+      <Header />
+      <Input />
+      <Container />
+      <Footer />
     </div>
   );
 }
