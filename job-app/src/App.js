869aa6c24faba3f8eaca5aa2585fd8cc48fd2c25
chiomaT <chiomanwabugwu11@yahoo.com>
2022-04-10 23:02:34 +0100

    changes made to the log in page

--- job-app/src/App.js
+++ job-app/src/App.js
@@ -1,16 +1,13 @@
-
 import React from "react";
-import ApplicationForm from "./Components/ApplicationForm";
-// import "./App.css";
 import CompanyLoginPage from "./Components/login-folder/CompanyLogin";
-import React from 'react';
-import ApplicationForm from './Components/ApplicationForm'
-import JobApplications from "./Components/JobCreationForm"
-import './App.css';
+import ApplicationForm from "./Components/ApplicationForm";
+import JobApplications from "./Components/JobCreationForm";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <CompanyLoginPage />
       <ApplicationForm />
       <JobApplications />
     </div>
