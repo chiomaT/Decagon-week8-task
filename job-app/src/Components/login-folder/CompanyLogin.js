a7c4b9673a063c67d3757132db7bae8af6b61547
chiomaT <chiomanwabugwu11@yahoo.com>
2022-04-10 22:39:25 +0100

    changes accepted

--- job-app/src/Components/login-folder/CompanyLogin.js
+++ job-app/src/Components/login-folder/CompanyLogin.js
@@ -4,66 +4,83 @@ import Pictwo from "../../Images/interviewlogo.png";
 import Picthree from "../../Images/Ellipse 2.png";
 import Picfour from "../../Images/Ellipse 3.png";
 import Picfive from "../../Images/Ellipse 4.png";
-import "./login.css"
+import Picsix from "../../Images/for employers.png";
+import "./login.css";
+
 const CompanyLoginPage = () => {
   return (
-
-    <div className="container-fluid" id="whole-login-page">
-        <div className="row">
-    <div className="log-in-container">
-        <div className ="col-md-7" id="interview-section">
+    <div className="whole-login-page">
+      <div className="log-in-container">
+        <div className="interview-section">
           <div className="company-logo">
-          <img src={Pic}/>
-        </div>
-
-        <div className="log-in-title">
-          <h1>
-            find the best
-            <br />
-            candidates for your
-            <br />
-            organisation
-          </h1>
-        </div>
-        <div className="interviewlogo">
-          <img src={Pictwo}/>
-        </div>
-        </div>
-        
-        <div className ="col-md-5" id="login">
-            <div class = "circleflex">
-                <div class ="none"></div>
-            <div className = "circle-img">
-            <div className ="blue-circle"> <img src={Picthree}/></div>
-           <div className = "red-circle"><img src={Picfour}/></div>
-           <div className = "pink-circle"><img src={Picfive}/></div>
-         </div>
+            <div id="find-jobs">
+              <img src={Pic} />
             </div>
-           <h1 className = "login-title">Login</h1>
-        <div id ="email-section">
-            <div id = "e">
-            <label  for="email">Email</label>
+            <div id="for-employers">
+              <img src={Picsix} />
             </div>
-            <div id = "e-input">
-            <input className="Email" id= "user-mail" type="text"/>
+          </div>
+          {/*company-logo  */}
+          <div className="interview-title">
+            <h1>
+              find the best
+              <br />
+              candidates for your
+              <br />
+              organisation
+            </h1>
+          </div>
+          {/*interview title  */}
+          <div className="interviewlogo">
+            <img src={Pictwo} />
+          </div>
+        </div>{" "}
+        {/* unknown */}
+        <div className="login">
+          <div class="circleflex">
+            <div class="none"></div>
+            <div className="circle-img">
+              <div className="blue-circle">
+                {" "}
+                <img src={Picthree} />
+              </div>
+              <div className="red-circle">
+                <img src={Picfour} />
+              </div>
+              <div className="pink-circle">
+                <img src={Picfive} />
+              </div>
             </div>
+            {/*circle-img  */}
+          </div>{" "}
+          {/*circle-flex */}
+          <h1 className="login-title">Login</h1>
+          <div id="email-section">
+            <div id="e">
+              <label for="email">Email</label>
+            </div>
+            <div id="e-input">
+              <input className="Email" id="user-mail" type="text" />
+            </div>
+          </div>
+          {/*email-section  */}
+          <div id="password-section">
+            <div id="p">
+              <label for password="password">
+                Password
+              </label>
+            </div>
+            <div id="p-input">
+              <input className="Password" type="text" />
+            </div>
+          </div>
+          {/*password-section  */}
+          <div className="login-btn">Login</div>
         </div>
-    <div id = "password-section">
-    <div id ="p">
-    <label for password = "password">Password</label>
-            </div>
-        <div id = "p-input">
-        <input className ="Password" type="text"/>
-        </div>
+        {/*login  */}
+      </div>{" "}
+      {/*log-in-container  */}
     </div>
-        <div className= "login-btn">Login</div>
-        </div>{/*login  */}
-        </div> {/*log-in-container  */}
-        </div>
-      </div> 
-    
-     
-    
   );
 };
 
