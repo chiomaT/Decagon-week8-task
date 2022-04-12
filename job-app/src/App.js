f74c31986270dc2766bbb96b3113ccb0d2068264
durotemy <durotemy@gmail.com>
2022-04-12 16:55:17 +0100

    ok

--- job-app/src/App.js
+++ job-app/src/App.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import ApplicationForm from './Components/ApplicationForm'
 import './App.css';
+import Header from './Components/Admin/Header';
+import Input from './Components/Admin/Input';
+import Container from './Components/Admin/Container';
+import Footer from './Components/Admin/Footer';
+import LoginAndSignUp from './Components/Admin/Login';
+import Form from './Components/Admin/Form';
+
 
 function App() {
   return (
     <div className="App">
       <ApplicationForm />
+      <LoginAndSignUp />
+      <ApplicationForm />
+      <Header />
+      <Input />
+      <Container />
+      <Footer />
+      <ApplicationForm />
+      <Form />
     </div>
   );
 }
