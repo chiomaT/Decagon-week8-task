9a74f0522cb0021834c686c76542b25c3a77fac3
DhanteyUD <otseclinton60@gmail.com>
2022-04-14 17:34:59 +0100

    changes made

--- job-app/src/Routes/Form/ApplicationForm.js
+++ job-app/src/Routes/Form/ApplicationForm.js
@@ -1,15 +1,13 @@
-
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Img from '../../Images/upload icon.png';
 import './ApplicationForm.css';
 
 const ApplicationForm = () => {
-  
   const getFirstName = (e) => {
     const userValue = e.target.value;
     console.log(userValue);
-  }
+  };
 
   return (
     <div className="apply-job-container">
@@ -27,60 +25,30 @@ const ApplicationForm = () => {
         </div>
 
         <label for="Last Name">First Name</label>
-        <input className="firstName" type="text" 
-        onChange={getFirstName} />
+        <input className="firstName" type="text" /*onChange={getFirstName}*/ />
         <label for="Last Name">Last Name</label>
-        <input className="lastName" type="text" 
-        onChange={getLastName} />
+        <input className="lastName" type="text" /*onChange={getLastName}*/ />
         <label for="Email">Email Address</label>
-        <input className="email" type="email" onChange={getEmail} />
+        <input className="email" type="email" /*onChange={getEmail}*/ />
         <label for="Location">Location</label>
-        <input className="location" type="text" onChange={getLocation} />
+        <input className="location" type="text" /*onChange={getLocation}*/ />
         <label for="Phone Number">Phone Number</label>
-        <input className="phoneNumber" type="tel" onChange={getPhone} />
-        <div className = "upload">
-          <img src={Imgg} alt="upload image"></img>
-          <p className="cv-content">Drag and drop your CV here</p>
-          <p className="cv-option">or</p>
-          <div className="cv-files">
-          <p className="cv-upload"
-          onChange={getCv}><a href=''>Browse Files</a></p>
-
-        <label className="apply-job-title" for="Last Name">
-          First Name
-        </label>
-        <input className="apply-job-input firstName" type="text" />
-        <label className="apply-job-title" for="Last Name">
-          Last Name
-        </label>
-        <input className="apply-job-input lastName" type="text" />
-        <label className="apply-job-title" for="Email">
-          Email Address
-        </label>
-        <input className="apply-job-input email" type="email" />
-        <label className="apply-job-title" for="Location">
-          Location
-        </label>
-        <input className="apply-job-input location" type="text" />
-        <label className="apply-job-title" for="Phone Number">
-          Phone Number
-        </label>
-        <input className=" apply-job-input phoneNumber" type="number" />
+        <input className="phoneNumber" type="tel" /*onChange={getPhone}*/ />
         <div className="upload">
           <img src={Img} alt="upload image"></img>
           <p className="cv-content">Drag and drop your CV here</p>
           <p className="cv-option">or</p>
           <div className="cv-files">
-            <p className="cv-upload">
+            <p className="cv-upload" /*onChange={getCv}*/>
               <a href="">Browse Files</a>
             </p>
           </div>
+          <input
+            className="Application-btn"
+            type="submit"
+            value="Submit Application"
+          />
         </div>
-        <input
-          className="Application-btn"
-          type="submit"
-          value="Submit Application"
-        />
       </form>
     </div>
   );
